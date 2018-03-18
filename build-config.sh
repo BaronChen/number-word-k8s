c56ffa77b33fda22f108fbf6e120b2f6e30e8114
Baron Chen <baron.zhongyangchen@gmail.com>
2018-03-18 20:33:52 +1100

    prepare for actual auto deploy

--- build-config.sh
+++ build-config.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cat api/backend-deployment.yml | sed "s/{apiVersion}/$1/g" > config.yml
-printf  '\n---\n' >> config.yml
-cat api/backend-service.yml >> config.yml
-printf  '\n---\n' >> config.yml
-cat web/frontend-deployment.yml | sed "s/{webVersion}/$2/g" >> config.yml
-printf  '\n---\n' >> config.yml
-cat web/frontend-service.yml >> config.yml
+cat api/backend-deployment.yml | sed "s/{apiVersion}/$1/g" > kube-config.yml
+printf  '\n---\n' >> kube-config.yml
+cat api/backend-service.yml >> kube-config.yml
+printf  '\n---\n' >> kube-config.yml
+cat web/frontend-deployment.yml | sed "s/{webVersion}/$2/g" >> kube-config.yml
+printf  '\n---\n' >> kube-config.yml
+cat web/frontend-service.yml >> kube-config.yml
 
 
