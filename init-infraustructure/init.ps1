5a05455e4bce69cbb251eb46f885f832c69e0c06
Baron Chen <baron.zhongyangchen@gmail.com>
2018-03-18 21:40:31 +1100

    working configs

---
+++ init-infraustructure/init.ps1
@@ -0,0 +1,10 @@
+
+az aks create --resource-group number-word --name number-word --node-count 1 --generate-ssh-keys
+
+az aks get-credentials --resource-group number-word --name number-word
+
+$CLIENT_ID = az aks show --resource-group number-word --name number-word --query "servicePrincipalProfile.clientId"
+
+$ACR_ID = az acr show --name baron123 --resource-group ContainerRegistry --query "id"
+
+az role assignment create --assignee $CLIENT_ID --role Reader --scope $ACR_ID
