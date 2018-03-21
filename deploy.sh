8dabbd6aa9300e29d7a6422c9b2ba9687d9e18ab
Baron Chen <baron.zhongyangchen@gmail.com>
2018-03-21 19:48:17 +1100

    Remove username and password

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-az login --service-principal -u 39afa0d5-fafd-49ae-b84d-d5e94c5def69 --password fa526938-9d11-4b52-b49d-07d0f87d9cee --tenant 0a7ce75d-d228-405f-88bc-3fcbc1447f2c
+az login --service-principal -u $SERVICE_PRINCIPLE --password $SP_PASSWORD --tenant $TENANT
 
 az aks get-credentials --resource-group number-word-prod --name number-word
 
