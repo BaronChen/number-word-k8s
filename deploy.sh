4eb802e45c15128b2eabc1730a56dee4e1f3c801
Baron Chen <baron.zhongyangchen@gmail.com>
2018-03-21 19:46:55 +1100

    Add circle ci deploy

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 
+az login --service-principal -u 39afa0d5-fafd-49ae-b84d-d5e94c5def69 --password fa526938-9d11-4b52-b49d-07d0f87d9cee --tenant 0a7ce75d-d228-405f-88bc-3fcbc1447f2c
+
+az aks get-credentials --resource-group number-word-prod --name number-word
+
+kubectl apply -f ./kube-config.yml
--- init-infraustructure/init.ps1
+++ init-infraustructure/init.ps1
@@ -1,9 +1,9 @@
 
-az aks create --resource-group number-word --name number-word --node-count 1 --generate-ssh-keys
+# az aks create --resource-group number-word-prod --name number-word --node-count 1 --generate-ssh-keys
 
-az aks get-credentials --resource-group number-word --name number-word
+az aks get-credentials --resource-group number-word-prod --name number-word
 
-$CLIENT_ID = az aks show --resource-group number-word --name number-word --query "servicePrincipalProfile.clientId"
+$CLIENT_ID = az aks show --resource-group number-word-prod --name number-word --query "servicePrincipalProfile.clientId"
 
 $ACR_ID = az acr show --name baron123 --resource-group ContainerRegistry --query "id"
 
