2cf526c1a80af2cbc1a5a7f4db9281d1870c096b
BaronChen <BaronChen@users.noreply.github.com>
2018-06-06 13:28:33 +1000

    Update deploy-gcloud.sh

--- deploy-gcloud.sh
+++ deploy-gcloud.sh
@@ -5,7 +5,7 @@ gcloud components install kubectl
 
 echo $GCLOUD_SERVICE_KEY > ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account --key-file=${HOME}/gcloud-service-key.json
-gcloud config set project number-text-convert
+gcloud config set project number-2-text
 gcloud config set compute/zone australia-southeast1-a
 
 gcloud container clusters get-credentials number-text-convert
